3122d1a5b7eb9729b08f055b8b4f3464189867f6
Haris Elfian <harisdj1996@gmail.com>
2023-08-24 15:18:24 +0700

    modif

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -2,6 +2,7 @@ using HarisPizza.Models;
 using HarisPizza.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using System.Runtime.Remoting;
 
 namespace HarisPizza.Controllers;
 
@@ -11,7 +12,6 @@ public class PizzaController : ControllerBase
 {
     public PizzaController()
     {
-
     }
 
     // GET all action
@@ -32,8 +32,43 @@ public class PizzaController : ControllerBase
     }
 
     // POST action
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        return CreatedAtAction(nameof(Get), new { id = pizza.Id }, pizza);
+        // This code will save the pizza and return a result
+    }
 
     // PUT action
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest();
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza is null)
+            return NotFound();
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+        // This code will update the pizza and return a result
+    }
 
     // DELETE action
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza is null)
+            return NotFound();
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+        // This code will delete the pizza and return a result
+    }
 }
